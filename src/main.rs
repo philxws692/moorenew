74694db51555850575bbe043e657b3e524ffb2dc
philxws692 <63463378+philxws692@users.noreply.github.com>
2025-03-25 13:00:36 +0100

    refactor: moved logging to own module

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod utils;
 
+use crate::utils::logging;
 use crate::utils::sftp::SftpClient;
 use dotenv::dotenv;
 use std::env;
-use std::env::VarError;
-use tracing::{error, info};
-use tracing_subscriber::fmt::Subscriber;
 
 fn main() {
     dotenv().ok();
 
-    setup_logging();
+    logging::setup_logging();
 
     let username = &env::var("SFTP_USERNAME").unwrap()[..];
     let host = &env::var("SFTP_HOST").unwrap()[..];
@@ -18,33 +16,4 @@ fn main() {
     let public_key_path = &env::var("PUBLIC_KEY_PATH").unwrap()[..];
 
     let client = SftpClient::connect(username, host, private_key_path, public_key_path).unwrap();
-
-
-}
-
-fn setup_logging() {
-    let structured_logging_key = "STRUCTURED_LOGGING";
-    match env::var(structured_logging_key) {
-        Ok(value) => {
-            if value == "true" {
-                // Structured logging enabled
-
-                let subscriber = tracing_subscriber::fmt().json().finish();
-                tracing::subscriber::set_global_default(subscriber).unwrap();
-
-                info!("logging is set to {}", structured_logging_key);
-            } else {
-                tracing_subscriber::fmt().init();
-                info!("structured logging disabled");
-            }
-        }
-        Err(_) => {
-            unsafe {
-                env::set_var(structured_logging_key, "false");
-            }
-            assert_eq!(env::var(structured_logging_key), Ok("false".to_string()));
-            tracing_subscriber::fmt().init();
-            info!("structured logging disabled");
-        }
-    }
 }
---
+++ src/utils/logging.rs
@@ -0,0 +1,42 @@
+use std::env;
+use tracing::info;
+use tracing::level_filters::LevelFilter;
+
+/// setup_logging sets the logging up, based on the set environment variables. If the variable
+/// `STRUCTURED_LOGGING` is set, the logger will output the logs in JSON format, ready to
+/// ingest into any SIEM or monitoring solution. If the variable is not set it will pretty print
+/// the log messages.
+/// # Examples
+/// ```rs
+/// use crate::utils::logging;
+///
+/// fn main() {
+///     logging::setup_logging();
+/// }
+/// ```
+pub fn setup_logging() {
+    let structured_logging_key = "STRUCTURED_LOGGING";
+    match env::var(structured_logging_key) {
+        Ok(value) => {
+            if value == "true" {
+                // Structured logging enabled
+
+                let subscriber = tracing_subscriber::fmt().json().with_max_level(LevelFilter::DEBUG).finish();
+                tracing::subscriber::set_global_default(subscriber).unwrap();
+
+                info!("logging is set to {}", structured_logging_key);
+            } else {
+                tracing_subscriber::fmt().with_max_level(LevelFilter::DEBUG).init();
+                info!("structured logging disabled");
+            }
+        }
+        Err(_) => {
+            unsafe {
+                env::set_var(structured_logging_key, "false");
+            }
+            assert_eq!(env::var(structured_logging_key), Ok("false".to_string()));
+            tracing_subscriber::fmt().with_max_level(LevelFilter::DEBUG).init();
+            info!("structured logging disabled");
+        }
+    }
+}
