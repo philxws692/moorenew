f43a7edebd1f348fc449945a0c2bd1b6777a0758
philxws692 <63463378+philxws692@users.noreply.github.com>
2025-04-04 14:52:34 +0200

    fix: extended configuration command

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -3,12 +3,15 @@ use std::io::Write;
 
 pub fn generate_config() {
     let config_string = "STRUCTURED_LOGGING=false
-PRIVATE_KEY_PATH=rustrover
-PUBLIC_KEY_PATH=rustrover.pub
-SFTP_USERNAME=root
-SFTP_HOST=";
+PRIVATE_KEY_PATH=moorenew
+PUBLIC_KEY_PATH=moorenew.pub
+SFTP_USERNAME=docker
+SFTP_HOST=127.0.0.1
+SFTP_PORT=22
+NPM_CERT_PATH=/path/to/nginx/cert
+MAILCOW_CERT_PATH=/opt/mailcow-dockerized/data/assets/ssl";
 
-    let mut file = File::create(".env.moorenew.moorenew").unwrap();
+    let mut file = File::create(".env.moorenew").unwrap();
     file.write_all(config_string.as_bytes()).unwrap();
 
 }
