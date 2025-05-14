946d894a0b8f61d47e61b6f6f51fddad17e95358
philxws692 <63463378+philxws692@users.noreply.github.com>
2025-05-14 13:14:20 +0200

    feat(cli): updated cli to be more simple

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,115 @@
-mod args;
-mod utils;
 mod system;
+mod utils;
 
-use crate::args::Action;
+use crate::system::serviceproviders::ServiceProvider;
 use crate::utils::certificates::download_certificates;
 use crate::utils::config::generate_config;
 use crate::utils::logging;
 use crate::utils::ssh::SSHClient;
 use crate::utils::sshkeygen;
 use crate::utils::sysinfo;
-use args::MooRenewArgs;
-use clap::Parser;
+use clap::{arg, Command};
 use std::env;
-use std::io::Error;
 use std::path::Path;
-use std::process::exit;
 use std::time::Duration;
 use tokio::time::sleep;
 use tracing::{error, info, instrument};
-use crate::system::serviceproviders::ServiceProvider;
 
 #[tokio::main]
 async fn main() {
+    dotenv::from_filename(".env.moorenew").ok();
+    logging::setup_logging();
+
+    let args = Command::new("moorenew")
+        .subcommand(
+            Command::new("keygen")
+                .about("Generate a SSH Keypair which MooRenew uses to fetch the certificates")
+                .args([
+                    arg!(-a --algorithm <algorithm> "The algorithm to use for the keypair. Defaults to ed25519. Can also be set to rsa4096 for RSA keypairs"),
+                    arg!(-c --comment <comment> "The comment to use for the keypair. Defaults to the username@hostname of the current user"),
+                    arg!(-f --filename <filename> "The filename to use for the keypair. Defaults to moorenew"),
+                ])
+        )
+        .subcommand(
+            Command::new("service")
+                .about("Used to manage the moorenew service")
+                .subcommand(
+                    Command::new("setup")
+                        .about("Used to create the needed service setup files")
+                )
+        )
+        .subcommand(
+            Command::new("run")
+                .about("Run the update process")
+                .arg(
+                    arg!(-d --dry-run "Don't actually update the certificates, just print what would happen"),
+                )
+        )
+        .get_matches();
+
+    if let Some(args) = args.subcommand_matches("keygen") {
+        let mut algorithm = args
+            .get_one::<String>("algorithm")
+            .map(String::as_str)
+            .unwrap_or("ed25519");
+        let arg_comment = args
+            .get_one::<String>("comment")
+            .map(String::as_str)
+            .unwrap_or("");
+        let filename = args
+            .get_one::<String>("filename")
+            .map(String::as_str)
+            .unwrap_or("moorenew");
+
+        let comment: String;
+
+        if arg_comment == "" {
+            comment = format!(
+                "{}@{}",
+                sysinfo::get_loggedin_user(),
+                sysinfo::get_hostname()
+            );
+        } else {
+            comment = arg_comment.to_string();
+        }
+
+        if algorithm != "rsa4096" && algorithm != "ed25519" {
+            eprintln!("{} is not a valid algorithm, using ed25519", algorithm);
+            algorithm = "ed25519";
+        }
+
+        sshkeygen::generate_rsa_keypair(&algorithm, &filename, &comment);
+    }
+
+    if let Some(subcommand) = args.subcommand_matches("service") {
+        if let Some(_) = subcommand.subcommand_matches("setup") {
+            match system::services::create_service_files("moorenew", ServiceProvider::SYSTEMD) {
+                Ok(_) => {
+                    println!("successfully created service files");
+                }
+                Err(_) => {
+                    eprintln!("failed to create service files");
+                }
+            }
+        }
+    }
+
+    if let Some(args) = args.subcommand_matches("run") {
+        let dry_run = args.get_flag("dry-run");
+        if dry_run {
+            println!("running in dry run mode");
+        } else {
+            println!("running in normal mode");
+        }
+        update_certificates(dry_run);
+    }
+
     if !Path::new(".env.moorenew").exists() {
         println!("assuming first run due to missing config");
         println!("generating default config in .env.moorenew");
         generate_config();
     }
-    if !Path::new("moorenew.timer").exists() {
-        println!("assuming first run due to missing timer");
-        println!("generating default timer in moorenew.timer");
-        match system::services::create_service_files("moorenew", ServiceProvider::SYSTEMD) {
-            Ok(_) => {
-                println!("successfully created service files");
-            }
-            Err(_) => {
-                eprintln!("failed to create service files");
-            }
-        }
-    }
-    dotenv::from_filename(".env.moorenew").ok();
-    logging::setup_logging();
 
-    let args = MooRenewArgs::parse();
-
-    match args.action {
-        Some(Action::Keygen(keygen)) => {
-            // Checking for algorithm
-            let mut algo = "ed25519".to_string();
-            if let Some(algorithm) = keygen.algorithm {
-                if algorithm == "rsa4096" {
-                    algo = algorithm;
-                } else if algorithm != "ed25519" {
-                    error!("{} is not a valid algorithm", algorithm);
-                    exit(1);
-                }
-            }
-            info!("using {} for key generation", algo);
-
-            // Getting filename
-            let mut keyfilename = "moorenew".to_string();
-            if let Some(keyname) = keygen.filename {
-                keyfilename = keyname;
-            }
-            info!("using {} for key filename", keyfilename);
-
-            // Getting comment
-            let mut comment = format!(
-                "{}@{} (moorenew service)",
-                sysinfo::get_loggedin_user(),
-                sysinfo::get_hostname()
-            );
-            if let Some(comm) = keygen.comment {
-                comment = comm;
-            }
-            sshkeygen::generate_rsa_keypair(&algo, &keyfilename, &comment)
-        }
-        Some(Action::Run(run)) => {
-            if run.dry_run == false {
-                info!("starting update process...");
-                update_certificates(false);
-            } else {
-                info!("starting dry run...");
-                update_certificates(true);
-            }
-        }
-        _ => {}
-    }
-    
     sleep(Duration::from_millis(10)).await;
 }
 
