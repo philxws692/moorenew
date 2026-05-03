c6651ddac31ca71ba5e60932077b561589719309
philxws692 <63463378+philxws692@users.noreply.github.com>
2026-05-03 16:08:05 +0200

    chore: setup dev tooling

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Basic tooling needed for the development of moorenew";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rust-analyzer
+            cargo-machete
+            cargo-nextest
+            cargo-audit
+
+            just
+
+            git-cliff
+          ];
+        };
+      }
+    );
+}
